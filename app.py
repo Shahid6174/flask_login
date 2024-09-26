54e10f096452a16ec674cafeb8f5c7157a163e3c
Mohammed Shahid <mdshahid.mds25@gmail.com>
2024-09-26 17:22:13 +0530

    flask_login

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.secret_key = "secret_key"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///home.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+    
+    def set_password(self,password):
+        self.password_hash =  generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash ,password)
+    
+
+@app.route('/')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def about():
+    return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email).first()
+    if user and user.check_password(password):
+        session['email'] = email
+        return redirect(url_for('home'))  # Fixing this line
+
+    else:
+        return render_template('login.html', error="Wrong Password/Email!")
+    
+
+@app.route('/reg_validation', methods=['POST'])
+def reg_validation():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return render_template("register.html", error="User Already Here!")
+    else:
+        new_user = User(email=email)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['email'] = email
+        return redirect(url_for('home'))
+        
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
